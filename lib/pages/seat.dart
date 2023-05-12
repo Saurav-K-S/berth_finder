b0520e93c618adeb5a1f385a1df5606d4eacbd54
Saurav-K-S <67679062+Saurav-K-S@users.noreply.github.com>
2023-05-12 22:59:47 +0530

    seating numbering fixed

--- lib/pages/seat.dart
+++ lib/pages/seat.dart
@@ -45,58 +45,64 @@ class _SeatState extends State<Seat> {
                         fontWeight: FontWeight.w600),
                   )),
             ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Flexible(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-                      child: TextField(
-                        controller: myController,
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(8),
-                                    bottomLeft: Radius.circular(8))),
-                            hintText: 'Enter Seat Number',
-                            focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xff004E98)),
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(8),
-                                    bottomLeft: Radius.circular(8)))),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                        child: TextField(
+                          controller: myController,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(8),
+                                      bottomLeft: Radius.circular(8))),
+                              hintText: 'Enter Seat Number',
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Color(0xff004E98)),
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(8),
+                                      bottomLeft: Radius.circular(8)))),
+                        ),
                       ),
                     ),
-                  ),
-                  Flexible(
-                      child: SizedBox(
-                    height: 60,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xff004E98),
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadiusDirectional.only(
-                                    topEnd: Radius.circular(8),
-                                    bottomEnd: Radius.circular(8))),
-                            elevation: 0),
-                        onPressed: () {
-                          print(myController.text);
-                          setState(() {
-                            sel = List.filled(50, false);
-                            sel[(int.parse(myController.text) - 1)] =
-                                !sel[(int.parse(myController.text) - 1)];
-                          });
-                        },
-                        child: const Icon(Icons.arrow_right_alt_rounded)),
-                  ))
-                ]),
+                    Flexible(
+                        child: SizedBox(
+                      height: 60,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xff004E98),
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius: BorderRadiusDirectional.only(
+                                      topEnd: Radius.circular(8),
+                                      bottomEnd: Radius.circular(8))),
+                              elevation: 0),
+                          onPressed: () {
+                            print(myController.text);
+                            setState(() {
+                              sel = List.filled(50, false);
+                              sel[(int.parse(myController.text) - 1)] =
+                                  !sel[(int.parse(myController.text) - 1)];
+                            });
+                          },
+                          child: const Icon(Icons.arrow_right_alt_rounded)),
+                    ))
+                  ]),
+            ),
             SeatRow(sel: sel),
             SeatRow(sel: sel),
             SeatRow(sel: sel),
             SeatRow(sel: sel),
             SeatRow(sel: sel),
             SeatRow(sel: sel),
+            SizedBox(
+              height: 100,
+            )
           ],
         ),
       ),
@@ -114,38 +120,36 @@ class SeatRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 25),
-      child: Flexible(
-        child: Column(
-          children: [
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              SeatPos(number: seatno + 1, name: 'Window', select: sel[seatno]),
-              SeatPos(
-                  number: seatno + 2, name: 'Middle', select: sel[seatno + 1]),
-              SeatPos(
-                  number: seatno + 3, name: 'Aisle', select: sel[seatno + 2]),
-              const SizedBox(width: 40),
-              SeatPos(
-                  number: seatno + 4,
-                  name: 'Side\nUpper',
-                  select: sel[seatno + 3]),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              SeatPos(
-                  number: seatno + 8, name: 'Window', select: sel[seatno + 7]),
-              SeatPos(
-                  number: seatno + 7, name: 'Middle', select: sel[seatno + 6]),
-              SeatPos(
-                  number: seatno + 6, name: 'Aisle', select: sel[seatno + 5]),
-              const SizedBox(width: 40),
-              SeatPos(
-                  number: seatno + 5,
-                  name: 'Side\nLower',
-                  select: sel[seatno + 4])
-            ]),
-          ],
-        ),
+    return Flexible(
+      child: Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SeatPos(number: seatno + 1, name: 'Window', select: sel[seatno]),
+            SeatPos(
+                number: seatno + 2, name: 'Middle', select: sel[seatno + 1]),
+            SeatPos(number: seatno + 3, name: 'Aisle', select: sel[seatno + 2]),
+            const SizedBox(width: 40),
+            SeatPos(
+                number: seatno + 7,
+                name: 'Side\nUpper',
+                select: sel[seatno + 6]),
+          ]),
+          const SizedBox(
+            height: 50,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SeatPos(
+                number: seatno + 4, name: 'Window', select: sel[seatno + 3]),
+            SeatPos(
+                number: seatno + 5, name: 'Middle', select: sel[seatno + 4]),
+            SeatPos(number: seatno + 6, name: 'Aisle', select: sel[seatno + 5]),
+            const SizedBox(width: 40),
+            SeatPos(
+                number: seatno + 8,
+                name: 'Side\nLower',
+                select: sel[seatno + 7])
+          ]),
+        ],
       ),
     );
   }
